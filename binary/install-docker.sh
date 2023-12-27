94e1bdc7c77e8b9e6edd5fe3030007f265420f9b
johnseed <48501700+johnseed@users.noreply.github.com>
2023-12-27 11:34:49 +0800

    Update install-docker.sh

--- binary/install-docker.sh
+++ binary/install-docker.sh
@@ -1,6 +1,6 @@
 tar xzvf docker-20.10.9.tgz
 cp docker/* /usr/bin/
-dockerd &
+# dockerd & # manual start
 cp docker.service /etc/systemd/system
 cp docker.socket /etc/systemd/system
 systemctl enable docker.service
